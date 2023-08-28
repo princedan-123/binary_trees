74ec0eaf56c07ddbf8146e893de89ddfd6477132
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-28 15:32:12 +0000

    removed parents check for NULL

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,21 +10,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
+	binary_tree_t *newnode = NULL;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
 		return (NULL);
-	else
-	{
-		binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
-		if (newnode == NULL)
-			return (NULL);
-		newnode->n = value;
-		newnode->parent = parent;
-		newnode->left = NULL;
-		newnode->right = NULL;
-		if (parent->left != NULL)
-			parent->left = newnode;
-		else
-			parent->right = newnode;
-	}
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
 	return (newnode);
 }
