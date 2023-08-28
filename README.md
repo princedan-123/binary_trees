0fe353e1e960d21da17e99622a0d1db92b312bdc
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-28 14:11:43 +0000

    readme, header file and print function file

---
+++ README.md
@@ -0,0 +1,1 @@
+ALX task on binary trees in 
---
+++ binary_trees.h
@@ -0,0 +1,26 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+static size_t _height(const binary_tree_t *tree);
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+
+#endif /* _BINARY_TREES_H_ */
