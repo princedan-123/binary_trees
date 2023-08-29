ac7c2060b3ecf51ac4a099387909a5c6bd53c034
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-29 08:54:43 +0000

    first upload to my branch

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a newnode
+ *
+ * @parent: a pointer to the parent node
+ * @value: the data to be inserted into the newnode
+ * Return: a pointer to the node is returned else null is returned
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode = NULL;
+	if (parent == NULL)
+		return (NULL);
+	newnode = binary_tree_node(parent, value);
+	if (newnode == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		newnode->right = parent->right;
+		parent->right->parent = newnode;
+		parent->right = newnode;
+	}
+	else if (parent->right == NULL)
+		parent->right = newnode;
+	return (newnode);
+}
